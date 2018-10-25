a610e71733482c76dd6ac5623d4e1c2f5cf838e4
Irene Mutua <mutuamirene.gmail.com>
2018-10-25 10:45:21 +0300

    made the tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -101,9 +101,4 @@ $(function () {
         });
 
     });
-
-    /* TODO: Write a test that ensures when a new feed is loaded
-     * by the loadFeed function that the content actually changes.
-     * Remember, loadFeed() is asynchronous.
-     */
 }());
